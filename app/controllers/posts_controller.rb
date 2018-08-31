5658ca18c7c77f6ec7786c46c6893c05640235b3
shoaib0300 <shoaibsarwar187@gmail.com>
2018-08-31 11:59:36 +0500

    styling And Structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,16 @@ class PostsController < ApplicationController
     @post= Post.all.order('created_at DESC')
   end
   def new
+    @post = Post.new
   end
   def create
     @post = Post.new(post_params)
-    @post.save
 
+    if @post.save
     redirect_to @post
+    else
+      render 'new'
+      end
   end
   def show
     @post = Post.find(params[:id])
